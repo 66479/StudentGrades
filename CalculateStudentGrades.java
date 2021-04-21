4648b494a64707e26c19ba45208587e46571e800
66479 <82689866+66479@users.noreply.github.com>
2021-04-21 13:54:23 -0500

    Add files via upload

---
+++ CalculateStudentGrades.java
@@ -0,0 +1,29 @@
+package Package18;
+
+	
+class CalculateS
+{
+  public static void main(String arg[])
+  {
+    int n=5,avg=0;
+ 
+    int a[]=new int[n];
+ 
+    a[0]=97;
+ 
+    a[1]=78;
+ 
+    a[2]=89;
+ 
+    a[3]=45;
+ 
+    a[4]=97;
+ 
+    for(int i=0;i<n;i++)
+ 
+    avg=avg+a[i];
+ 
+    System.out.println("average of  ("+a[0]+","+a[1]+","+a[2]+","+a[3]+","+a[4]+")  is ="+ avg/n);
+ System.out.println("Average grades of classes is81.2 ");
+  }
+}
